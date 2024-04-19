21d7a42379624b11791f79bf483700f7cd04658d
zcodlab <zmamanir@uni.edu.pe>
2024-04-19 09:19:59 -0500

    add addToPosition,Search methods

--- src/main/java/uni/aed/linkedlist/ListaEnlazadaSimple.java
+++ src/main/java/uni/aed/linkedlist/ListaEnlazadaSimple.java
@@ -1,11 +1,57 @@
 package uni.aed.linkedlist;
 public class ListaEnlazadaSimple {
     Nodo head;
+    int lenght=0;
+    
     public void addFirst(int data){
         Nodo newNodo=new Nodo(data);
         newNodo.next=head;
         head=newNodo;
+        lenght++;
     }
+    public void addToPosition(int position,int currentDataValue){
+        Nodo anotherNodo=new Nodo(currentDataValue);        
+        Nodo current;
+        int index;
+        if((position<1 || position>lenght))
+           System.out.println("Posicion debe ser un valor entre 1-"+lenght);
+        else
+        {
+            if(position==1)
+                addFirst(anotherNodo.data);                
+            else
+            {
+                current=head;            
+                index=1;
+                while(index<(position-1)){
+                    current=current.next;
+                    index++;
+                }
+                anotherNodo.next=current.next;
+                current.next=anotherNodo;
+                lenght++;
+            }
+        }      
+    }
+    public int search(int key){
+       Nodo temp=head;
+       boolean isFound=false;
+       int index=1;
+       while((temp!=null) && (isFound==false)){
+           if(temp.data==key)
+               isFound=true;
+           else{
+               temp=temp.next;
+               index++;
+           }
+       }
+       if (isFound==true)
+           return index;
+       else
+           return -1;
+       
+    }
+    
     public void addLast(int data){
         Nodo newNodo=new Nodo(data);        
         if(head==null){
@@ -15,7 +61,8 @@ public class ListaEnlazadaSimple {
         Nodo current=head;
         while(current.next!=null)
             current=current.next;        
-        current.next=newNodo;            
+        current.next=newNodo; 
+        lenght++;
     }
     public void remove(int data){
         if(head==null)
@@ -23,6 +70,7 @@ public class ListaEnlazadaSimple {
         //si el elemento a eliminar es el primer nodo
         if(head.data==data){
             head=head.next;
+            lenght--;
             return;
         }
         //si el nodo a eliminar no es el primer nodo
@@ -30,8 +78,10 @@ public class ListaEnlazadaSimple {
         while(current.next!=null && current.next.data!=data)
             current=current.next;
         
-        if(current.next!=null)
+        if(current.next!=null){
             current.next=current.next.next;
+            lenght--;
+        }
     }
     public void clear(){
         head=null;
@@ -49,4 +99,18 @@ public class ListaEnlazadaSimple {
         }
         return lista;
     }
+    public boolean isEmpty(){
+        return(lenght==0);
+    }
+    public void displayReverse(){
+        Nodo temp=head;
+        if(isEmpty()==false)
+            reverse(temp);        
+    }
+    private void reverse(Nodo temp){
+        if(temp.next!=null)
+            reverse(temp.next);
+        System.out.println(temp.data);
+    }
+            
 }
--- src/main/java/uni/aed/linkedlist/ListaEnlazadaSimpleMain.java
+++ src/main/java/uni/aed/linkedlist/ListaEnlazadaSimpleMain.java
@@ -18,11 +18,22 @@ public class ListaEnlazadaSimpleMain {
         lista.addLast(10);
         lista.addLast(20);
         lista.addLast(30);
+        lista.addFirst(40);
+        lista.addToPosition(2,60);
         System.out.println("Visualizando el contenido de la lista");
         System.out.println(lista.toString());
         
-        System.out.println("Eliminando un elemento");
+        System.out.println("Eliminando un elemento: 20");
         lista.remove(20);
+        
+        System.out.println("Buscando el elemento: 60");
+        int result=lista.search(60);
+        if(result==-1)
+            System.out.println("El elemento no se ubico en la lista");
+        else
+            System.out.println("El elemento se ubico en la lista, en la posicion"+ result);
+            
+        
         System.out.println("Visualizando el contenido de la lista");
         System.out.println(lista.toString());
         

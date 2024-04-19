1e3f59e4295d03c0d96609e6e2c34ad467d64c6e
zcodlab <zmamanir@uni.edu.pe>
2024-04-19 10:09:39 -0500

    add OSimpleLinkedListMain.remove method

--- src/main/java/osimplelinkedlist/OSimpleLinkedListMain.java
+++ src/main/java/osimplelinkedlist/OSimpleLinkedListMain.java
@@ -18,7 +18,7 @@ public class OSimpleLinkedListMain {
             nombre=escaner.next();
             if(!nombre.equalsIgnoreCase("salir")){
                 b=new Bicicleta(nombre);
-                olista.addFirst(b);
+                olista.addLast(b);
             }
             else
                 break;
@@ -26,5 +26,13 @@ public class OSimpleLinkedListMain {
         System.out.println("Visualizar el contenido de la lista");
         System.out.println(olista.toString());
         
+        System.out.println("Eliminando un elemento de la lista");
+        System.out.println("Consigne el elemento a eliminar");
+        nombre=escaner.next();
+        b=new Bicicleta(nombre);
+        olista.remove(b);
+        
+        System.out.println("Visualizar el contenido de la lista");
+        System.out.println(olista.toString());
     }
 }
--- src/main/java/uni/aed/model/Bicicleta.java
+++ src/main/java/uni/aed/model/Bicicleta.java
@@ -1,5 +1,5 @@
 package uni.aed.model;
-public class Bicicleta {
+public class Bicicleta implements Comparable{
     private String nomPropietario;
 
     public Bicicleta(String nomPropietario) {
@@ -22,6 +22,11 @@ public class Bicicleta {
     public int compareTo(Bicicleta o){
         String p2name=o.getNomPropietario();
         return this.nomPropietario.compareTo(p2name);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return compareTo((Bicicleta) o);
     }
     
 }

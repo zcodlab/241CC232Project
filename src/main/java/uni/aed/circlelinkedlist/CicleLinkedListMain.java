4d192ac6fe3d4078fa0d48fbaba9a8a9b877b615
zcodlab <zmamanir@uni.edu.pe>
2024-04-26 10:19:35 -0500

    Add remove method

--- src/main/java/uni/aed/circlelinkedlist/CicleLinkedListMain.java
+++ src/main/java/uni/aed/circlelinkedlist/CicleLinkedListMain.java
@@ -13,5 +13,17 @@ public class CicleLinkedListMain {
         
         System.out.println("Visualizando el contenido de la lista");
         System.out.println(lista.toString());
+        
+        System.out.println("Buscando el valor 60");
+        if(lista.search(60))
+            System.out.println("El valor 60 fue ubicado en la lista");
+        else
+            System.out.println("El valor 60 No se ubico en la lista");
+        
+        System.out.println("Buscando el valor 20");
+        if(lista.search(20))
+            System.out.println("El valor 20 fue ubicado en la lista");
+        else
+            System.out.println("El valor 20 No se ubico en la lista");
     }
 }
--- src/main/java/uni/aed/circlelinkedlist/CircleLinkedList.java
+++ src/main/java/uni/aed/circlelinkedlist/CircleLinkedList.java
@@ -14,6 +14,39 @@ public class CircleLinkedList {
         current.next=newNodo;
         newNodo.next=head;
     }
+    
+    public boolean search(int key){
+        if(head==null)
+            return false;
+        Nodo current=head;
+        do{
+            if(current.data==key)
+                return true;            
+            current=current.next;
+        }while(current!=head);
+        return false;
+    }
+    public void remove(int data){
+        if(head==null)
+            return;
+        if(head.data==data){
+            Nodo current=head;
+            while(current.next!=head)
+                current=current.next;
+            head=head.next;
+            current.next=head;
+            return;
+        }
+        
+        Nodo current=head;
+        while(current.next!=head && current.next.data!=data)
+            current=current.next;
+        
+        if(current.next!=head)
+            current.next=current.next.next;
+        //falta analizar eliminacion de un nodo intermedio de una lista de mas de 3 elementos
+        //falta analizar eliminar del ultimo nodo de la lista
+    }
 
     @Override
     public String toString() {

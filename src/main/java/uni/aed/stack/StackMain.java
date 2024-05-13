452b89935a543ced51a005d46fe5d2c713ae6b0d
zcodlab <zmamanir@uni.edu.pe>
2024-05-13 10:52:40 -0500

    Modify StackMain class

--- src/main/java/uni/aed/stack/StackMain.java
+++ src/main/java/uni/aed/stack/StackMain.java
@@ -8,8 +8,18 @@ public class StackMain {
         StackTDA<String> pila=new ArrayStackTDA<>(3);
         pila.push("Jose");
         pila.push("Juan");
+        pila.push("Mario");
         System.out.println("Despues de ingresar elementos a la pila");
         System.out.println(pila.toString());
+        System.out.println("Realizando una llamada de tipo pop");
+        System.out.println(pila.pop());
+        System.out.println("Despues de realizar una llamado pop");
+        System.out.println(pila.toString());
+        System.out.println("Realizando una llamada de tipo peek");
+        System.out.println(pila.peek());
+        System.out.println("Despues de realizar una llamado peek");
+        System.out.println(pila.toString());
+        
     }
     
 }
